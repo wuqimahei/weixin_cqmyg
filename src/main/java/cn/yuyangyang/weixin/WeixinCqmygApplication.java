5116b5c8ff1a6b736c400e7127a8fe8bc9ab37ba
fish <yu@yuyangyang.cn>
2020-07-23 17:35:25 +0800

    init

---
+++ src/main/java/cn/yuyangyang/weixin/WeixinCqmygApplication.java
@@ -0,0 +1,13 @@
+package cn.yuyangyang.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinCqmygApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeixinCqmygApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yuyangyang/weixin/WeixinCqmygApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yuyangyang.weixin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeixinCqmygApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
